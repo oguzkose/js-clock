e283bdb56d348da1268f1cf40276a1f2648fe32e
oguzkose <oguzkose2019@outlook.com>
2021-07-22 19:10:38 +0300

    ss added

--- clock.js
+++ clock.js
@@ -5,6 +5,7 @@ function showTime() {
   let second = time.getSeconds();
   let day = time.getDay();
   let month = time.getMonth();
+  let allDate = time.toDateString()
   
 
   hours < 10 ? (hours = `0${hours}`) : hours;
@@ -14,8 +15,10 @@ function showTime() {
   month < 10 ? (month = `0${month}`) : month;
 
   let dateTimeNow = `${hours}:${minute}:${second}`;
-  let clock = document.querySelector("h2#clock");
+  let clock = document.querySelector("h1#clock");
+  let date = document.querySelector("h1#date")
   clock.innerHTML = dateTimeNow;
+  date.innerHTML = allDate;
 }
 showTime();
 setInterval(showTime, 1000);
